6ea63229a51b5e9c80db40fe8d1ce7576b6409c8
karthikasadheez <karthika.sadheez@gmail.com>
2023-01-20 12:24:13 -0500

    Demo

---
+++ JavaBasicsCoding/src/com/n2s/basics/Demo.java
@@ -0,0 +1,16 @@
+package com.n2s.basics;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+	    Example obj=new Example();
+        System.out.println(obj.age);
+        
+        Example2 obj1=new Example2();
+        System.out.println (obj1.multi(5, 8));
+	
+	
+	}
+
+}
