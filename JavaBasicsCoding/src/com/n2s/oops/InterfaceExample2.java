144dd45ffeca4ea30a7317573264989e83d213ea
karthikasadheez <karthika.sadheez@gmail.com>
2023-01-24 15:28:31 -0500

    Interface

---
+++ JavaBasicsCoding/src/com/n2s/oops/InterfaceExample2.java
@@ -0,0 +1,8 @@
+package com.n2s.oops;
+
+public interface InterfaceExample2 extends InterfaceExample3 {
+
+  public static final int InterfaceExample2_y=6;
+     public abstract int mult(int x,int y);
+	
+}
